bf9ec51224b96f3aeaf47509629905d3d3e2372c
Diego Ricardo Bonilla Villa <drbv27@gmail.com>
2025-05-21 22:13:12 -0500

    users query, error handling, loading handling, render users list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,94 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-import User from "./components/User";
+import axios from "axios";
+import User from "./components/UserCard";
+import UsersList from "./components/UsersList";
+import { MdErrorOutline } from "react-icons/md";
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+export interface Address {
+  city: string;
+  geo: Geo;
+  street: string;
+  suite: string;
+  zipcode: string;
+}
+
+export interface Company {
+  bs: string;
+  catchPhrase: string;
+  name: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: Address;
+  company: Company;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async (): Promise<void> => {
+      try {
+        const response = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(response.data);
+        setLoading(false);
+      } catch (error) {
+        if (error instanceof Error) {
+          setLoading(false);
+          setError(error.message);
+        } else {
+          setLoading(false);
+          setError("Hubo un error en la consulta");
+        }
+        console.log("error al consultar los usuarios");
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  //console.log(users);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col justify-center">
+        <h2>Loading....</h2>
+        <div className="loader"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <p>
+        <span className="text-red-500">
+          <MdErrorOutline />
+        </span>
+        Hubo un error...
+        <span className="text-red-500">
+          <MdErrorOutline />
+        </span>
+      </p>
+    );
+  }
+
   return (
     <>
-      <h1>Hola mi proyecto final</h1>
-      <User />
+      <UsersList users={users} />
     </>
   );
 }
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,45 @@
+import type { User } from "../App";
+
+interface UserCardProps {
+  user: User;
+}
+
+const UserCard = ({ user }: UserCardProps) => {
+  return (
+    <div className="border p-4 rounded-lg shadow-xl bg-gray-200">
+      <h2 className="text-xl font-semibold text-blue-900">{user.name}</h2>
+      <p className="text-gray-800">
+        <strong>Email:</strong> {user.email}
+      </p>
+      <p className="text-gray-800">
+        <strong>Username:</strong> {user.username}
+      </p>
+      <p className="text-gray-800">
+        <strong>Phone:</strong> {user.phone}
+      </p>
+      <p className="text-gray-800">
+        <strong>Website:</strong>{" "}
+        <a className="text-blue-600 hover:underline" href={user.website}>
+          {user.website}
+        </a>
+      </p>
+      <div>
+        <h3 className="text-md font-bold text-gray-800">Address:</h3>
+        <p className="text-gray-600 text-sm">
+          {user.address.street},{user.address.suite} <br />
+          {user.address.city},{user.address.zipcode}
+        </p>
+      </div>
+      <div>
+        <h3 className="text-md font-bold text-gray-800">Company:</h3>
+        <p className="text-gray-600 text-sm">
+          <span className="text-base text-gray-700">{user.company.name}</span>{" "}
+          <br />
+          <em className="text-gray-500">{user.company.catchPhrase}</em>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
